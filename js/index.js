462dee07048ddfe1bacbe7ca5db56056cba6b17a
VolkanicoDev <volkancmoll@gmail.com>
2024-03-26 18:59:40 +0100

    Modify: Add: Styles Footer

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.navbar__menu-toggle');
     const menuItems = document.querySelector('.navbar__navbar-right__menu-items');
     const closeBtn = document.querySelector('.navbar__close-btn');
+    menuItems.style.display = 'none';
 
     menuToggle.addEventListener('click', function () {
         menuItems.style.display = 'block';
         menuToggle.style.display = 'none';
         closeBtn.style.display = 'inline-block';
     });
-
     closeBtn.addEventListener('click', function () {
         menuItems.style.display = 'none';
         menuToggle.style.display = 'inline-block';
@@ -18,9 +18,5 @@ document.addEventListener('DOMContentLoaded', function () {
 window.addEventListener('resize', function() {
     if (window.innerWidth > 1000) {
         location.reload();
-    }
+    }    
 });
-
-
-  
-
